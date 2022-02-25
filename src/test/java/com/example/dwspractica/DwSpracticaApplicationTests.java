68f10c3895aff1225feb8e934a02f0d4bdfffefb
servgo <sergioveguegonzalez@gmail.com>
2022-02-25 12:21:05 +0100

    Initial commit

---
+++ src/test/java/com/example/dwspractica/DwSpracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dwspractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwSpracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
