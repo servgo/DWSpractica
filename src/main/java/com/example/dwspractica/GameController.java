7a3288cee35b5f8e7a10ccf13d873901929e14ad
servgo <sergioveguegonzalez@gmail.com>
2022-03-07 23:19:46 +0100

    prueba2

--- src/main/java/com/example/dwspractica/GameController.java
+++ src/main/java/com/example/dwspractica/GameController.java
@@ -3,7 +3,6 @@ package com.example.dwspractica;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,27 +13,35 @@ import javax.annotation.PostConstruct;
 public class GameController {
     @Autowired
     GameService gameService;
+
     @PostConstruct
-    public void init(){
+    public void init() {
         gameService.addGame(new Game("Elden Ring", "PC", 60));
     }
+
     @GetMapping("/addedGame")
-    public String newGame(@RequestParam String name, @RequestParam String platform, @RequestParam float price){
+    public String newGame(@RequestParam String name, @RequestParam String platform, @RequestParam float price) {
         gameService.addGame(new Game(name, platform, price));
         return "CreatedGame";
     }
+
     @GetMapping("/allGames")
-    public String allGames(Model model){
+    public String allGames(Model model) {
         model.addAttribute("games", gameService.getGames());
         return "ShowGames";
     }
+
     @GetMapping("/game/{id}")
-    public String showGame(Model model, @PathVariable int id){
+    public String showGame(Model model, @PathVariable int id) {
         model.addAttribute("game", gameService.getGames(id));
+        if (gameService.getGames(id) == null) {
+            return "error/401";
+        }
         return "ShowGame";
     }
+
     @GetMapping("/deleted/{id}")
-    public String deleteGame(@PathVariable int id){
+    public String deleteGame(@PathVariable int id) {
         gameService.deleteGame(id);
         return "DeletedGame";
     }
--- src/main/java/com/example/dwspractica/ShoppingCartController.java
+++ src/main/java/com/example/dwspractica/ShoppingCartController.java
@@ -1,7 +1,52 @@
 package com.example.dwspractica;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ShoppingCartController {
+    @Autowired
+    GameService gameService;
+    ShoppingCart shoppingCart;
+
+    @GetMapping("/addedToCart")
+    public String newGameToCart(Model model, @RequestParam int id) {
+        shoppingCart.addGame(this.gameService.getGames(id));
+        return "AddedToCart";
+    }
+
+    @RequestMapping("/ShoppingCart")
+    public String showShoppingCart(Model model) {
+
+        model.addAttribute("cart", shoppingCart);
+
+        if (shoppingCart.esVacia()) {
+            model.addAttribute("empty", true);
+        } else {
+            model.addAttribute("empty", false);
+        }
+
+        float sum = shoppingCart.getPrecio();
+
+        model.addAttribute("sum", sum);
+        return "ShoppingCart";
+    }
+
+    @RequestMapping("/RemoveGame")
+    public String removeGameShoppingCart(Model model,@RequestParam Game game) {
+        shoppingCart.deleteGame(game);
+        return "RemovedGame.html";
+    }
+
+    @RequestMapping("/RemoveShoppingCart")
+    public String removeShoppingCart(Model model) {
+        shoppingCart.clearCart();
+        return "RemovedShoppingCart.html";
+    }
 }
+
+
