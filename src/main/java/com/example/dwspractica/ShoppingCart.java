c6520fe8d4504bd38649ca5e389076256af17d3e
Dano19186 <92856939+Dano19186@users.noreply.github.com>
2022-03-07 16:23:03 +0100

    cart

--- src/main/java/com/example/dwspractica/ShoppingCart.java
+++ src/main/java/com/example/dwspractica/ShoppingCart.java
@@ -7,26 +7,25 @@ import java.util.Collection;
 import java.util.List;
 
 public class ShoppingCart {
-    private float precio;
+    private float price=0;
     private List<Game> games = new ArrayList<>();
 
     public float getPrecio(){
-        float sum = 0;
-        for(Game game: games){
-            sum+=game.getPrice();
-        }
-        return sum;
+        return this.price;
     }
 
     public void addGame (Game game){
         games.add(game);
+        this.price+=game.getPrice();
     }
 
     public void deleteGame (Game game){
         games.remove(game);
+        this.price-=game.getPrice();
     }
 
     public void clearCart (){
         this.games.clear();
+        this.price=0;
     }
 }
