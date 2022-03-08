e85f8f1fbead74208f68f0c91a672c575314fcfe
servgo <sergioveguegonzalez@gmail.com>
2022-03-08 10:36:57 +0100

    prueba2

--- src/main/java/com/example/dwspractica/RatingController.java
+++ src/main/java/com/example/dwspractica/RatingController.java
@@ -3,12 +3,10 @@ package com.example.dwspractica;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class RatingController {
@@ -21,21 +19,16 @@ public class RatingController {
     //We can see a game's ratings. If it doesn't exist, we'll see an error
     @RequestMapping("Game/{id}/Ratings")
     public String showRating(Model model, @PathVariable int id) {
-        model.addAttribute("id", id);
+        model.addAttribute("ratings", ratingService.getRatings(id));
         if (gameService.getGames(id) == null) {
             return "error/401";
-        }
-        if (gameService.getGames(id) == null) {
-            model.addAttribute("empty", true);
         } else {
-            model.addAttribute("empty", false);
+            return "ShowRatings";
         }
-        model.addAttribute("ratings", ratingService.getRatings(id));
-        return "ShowRatings";
     }
 
     //We'll be redirected to "NewRating" related to a game and it's id
-    @RequestMapping("/Game/{id}/Ratings/CreateRating")
+    @GetMapping("/Game/{id}/Ratings/CreateRating")
     public String ratingCreation(Model model, @PathVariable int id) {
         if (gameService.getGames(id) == null) {
             return "error/401";
@@ -46,12 +39,20 @@ public class RatingController {
     }
 
     //We are able to create ratings, adding a title, comments and a rating with stars
-    @RequestMapping("/NewRating")
-    public String newRating(Model model, @RequestParam String title, @RequestParam String comment, @RequestParam int stars, @RequestParam int id) {
-
+    @GetMapping("/NewRating/{id}")
+    public String newRating(@RequestParam String title, @RequestParam String comment, @RequestParam int stars, @PathVariable int id) {
         Rating aux = new Rating(stars, title, comment);
+        aux.setId(id);
         ratingService.addRating(id, aux);
-        return "CreatedRating.html";
+        return "CreatedRating";
     }
+
+    @GetMapping("/deleteRating/{id}")
+    public String deleteRating(Model model, @PathVariable int id) {
+        model.addAttribute("id", id);
+        ratingService.deleteRating(id);
+        return "DeletedRating";
+    }
+
 
 }
