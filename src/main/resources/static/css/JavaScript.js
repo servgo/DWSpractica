c79dd2ee3f27856e499dd2896f38c3546949b038
servgo <sergioveguegonzalez@gmail.com>
2022-03-11 02:25:19 +0100

    prueba2

--- src/main/resources/static/css/JavaScript.js
+++ src/main/resources/static/css/JavaScript.js
@@ -1,8 +1,29 @@
-function back(){
-        history.back();
+function back() {
+    history.back();
 }
 
-function refresh()
-{
-        window.location.reload();
+function refresh() {
+    window.location.reload();
+}
+
+var num = 1;
+
+function nextGallery() {
+    num++;
+    if (num > 3) {
+        num = 1;
+    } else {
+        var photo = document.getElementById("photo");
+        photo.src = "/css/photo" + num + ".jpg";
+    }
+}
+
+function backGallery() {
+    num--;
+    if (num < 1) {
+        num = 3;
+    } else {
+        var photo = document.getElementById("photo");
+        photo.src = "/css/photo" + num + ".jpg";
+    }
 }
